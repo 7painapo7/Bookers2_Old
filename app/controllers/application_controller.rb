d575acb6f59b7573ac1035f1ddeb536ed96546dc
にしゆうき <nishiyuki@nishiyuukinoMacBook-Air.local>
2019-09-16 19:35:26 +0900

    [Add]マイページへの遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 	end
+
+	before_action :authenticate_user!
+
+	def after_sign_in_path_for(resource)
+	user_path(resource) # ログイン後に遷移するpathを設定
+	end
+
+	def after_sign_out_path_for(resource)
+	root_path # ログアウト後に遷移するpathを設定
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
 	def start
 	end
+	def new
+	end
+	def show
+	end
 end
